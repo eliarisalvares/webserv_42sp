06ca6eb3ec04054a119d3419c9a244b464c6ae03
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-11-07 14:41:42 -0300

    feat: opening file

--- webserver/src/openFile.cpp
+++ webserver/src/openFile.cpp
@@ -6,26 +6,26 @@
 /*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:21:55 by feralves          #+#    #+#             */
-/*   Updated: 2023/11/07 14:30:54 by feralves         ###   ########.fr       */
+/*   Updated: 2023/11/07 14:41:09 by feralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
 
-// int	openInFile(char *fileName, std::ifstream &input)
-// {
-// 	input.open(fileName);
-// 	if (!input.is_open())
-// 	{
-// 		errorInput("Can't open input file");
-// 		return (false);
-// 	}
-// 	return (true);
-// }
-
+int	openInFile(char *fileName, std::ifstream &input)
+{
+	input.open(fileName);
+	if (!input.is_open())
+	{
+		std::cout << "Can't open file" << std::endl;
+		return (false);
+	}
+	return (true);
+}
 
 int	getPort(char *fileName) {
-	// std::ifstream	inFile;
-	(void)fileName;
+	std::ifstream	inFile;
+	if (!openInFile(fileName, inFile))
+		return (false);
 	return (PORT);
 }
