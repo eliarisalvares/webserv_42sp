8c496ebcc2f5464f25f87906bda636a188de0071
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-11-09 11:11:25 -0300

    fix: logger messages end with end line

--- webserver/src/Logger/Logger.cpp
+++ webserver/src/Logger/Logger.cpp
@@ -12,26 +12,26 @@ Logger const& Logger::operator=(Logger const & copy) {
 }
 
 void Logger::error(std::string const msg) {
-	std::cerr << ORANGE << "[ ERROR ] " << msg << RESET;
+	std::cerr << ORANGE << "[ ERROR ] " << msg << RESET << std::endl;
 }
 
 void Logger::warning(std::string const msg) {
-	std::cout << YELLOW << "[ WARNING ] " << msg << RESET;
+	std::cout << YELLOW << "[ WARNING ] " << msg << RESET << std::endl;
 }
 
 void Logger::info(std::string const msg) {
-	std::cout << BLUE << "[ INFO ] " << msg << RESET;
+	std::cout << BLUE << "[ INFO ] " << msg << RESET << std::endl;
 }
 
 void Logger::debug(std::string const msg) {
 	if (DEBUG)
-		std::cout << GREY << "[ DEBUG ] " << msg << RESET;
+		std::cout << GREY << "[ DEBUG ] " << msg << RESET << std::endl;
 }
 
 void Logger::success(std::string const msg) {
-	std::cout << GREEN << msg << RESET;
+	std::cout << GREEN << msg << RESET << std::endl;
 }
 
 void Logger::title(std::string const msg) {
-	std::cout << PURPLE << msg << RESET;
+	std::cout << PURPLE << msg << RESET << std::endl;
 }
