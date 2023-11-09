71ed4bf1a3a7ad032f81f71797f33929499880bc
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-11-09 10:11:58 -0300

    Add port in Server

--- webserver/src/Server/Server.hpp
+++ webserver/src/Server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:50:47 by feralves          #+#    #+#             */
-/*   Updated: 2023/11/09 01:30:04 by sguilher         ###   ########.fr       */
+/*   Updated: 2023/11/09 10:02:04 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,15 @@ class Server {
 		Server& operator=(Server const& copy);
 
 		int		getSocket(void) const;
+		int		getPort(void) const;
 		void	setSocket(int port);
 		int		getBufferSize(void) const;
 		void	setBufferSize(int size);
 
 	private:
+		int	_port;
 		int	_socket;
-		int _bufferSize;
+		int	_bufferSize;
 };
 
 #endif
