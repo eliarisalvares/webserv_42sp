f01efe1541f0593f3d2f9c84d5d2c4821d4538a0
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-11-09 10:20:23 -0300

    fix: Request

--- webserver/src/Request/Request.cpp
+++ webserver/src/Request/Request.cpp
@@ -6,14 +6,14 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 19:30:54 by sguilher          #+#    #+#             */
-/*   Updated: 2023/11/09 03:15:56 by sguilher         ###   ########.fr       */
+/*   Updated: 2023/11/09 10:19:41 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
-Request::Request(void): _method(GET), _fd(0) { }
-Request::Request(int fd): _method(GET), _fd(fd) { }
+Request::Request(void): _fd(0), _method(GET) { }
+Request::Request(int fd): _fd(fd), _method(GET) { }
 
 Request::~Request(void) { }
 
@@ -29,6 +29,10 @@ Request const& Request::operator=(Request const & copy) {
 	return *this;
 }
 
-requestMethod	Request::getMethod(void) const {
+requestMethod Request::getMethod(void) const {
 	return this->_method;
+}
+
+int Request::fd(void) const {
+	return this->_fd;
 }
