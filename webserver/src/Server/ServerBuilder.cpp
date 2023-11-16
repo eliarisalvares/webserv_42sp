1e374e9b7926bc21e8e5834bacbd6eb34bde0dd0
ferfahl <fe.alves.fahl@gmail.com>
2023-11-16 15:26:40 -0300

    fix: parsing with one port

--- webserver/src/Server/ServerBuilder.cpp
+++ webserver/src/Server/ServerBuilder.cpp
@@ -1,1 +1,11 @@
 #include "ServerBuilder.hpp"
+
+void	getServerInfo(std::vector<std::string> input, int index) {
+	for (size_t i = index; i < input.size(); i++) {
+		if (input[i].substr() == "server {")
+			i++ ;
+		if (input[i].substr() == "}")
+			break ;
+		std::cout << "line " << i << " input : " << input[i] << std::endl;
+	}
+}
