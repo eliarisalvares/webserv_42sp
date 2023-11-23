8d70eb6b639b6382e223cd2ee87aa65211c1a198
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-11-23 00:47:25 -0300

    fix: first line parse working

--- webserver/src/Request/RequestParser.cpp
+++ webserver/src/Request/RequestParser.cpp
@@ -39,15 +39,18 @@ void RequestParser::break_data(char* buffer, size_t bytes_read) {
 		_data.push_back(buffer[i]);
 
 	std::vector<char>::iterator it, end = _data.end();
-	std::cout << GREY << "Received: ";
-	for(it = _data.begin(); it != end; ++it) {
-		std::cout << *it;
-		if (*it == CR)
-			std::cout << "\nCR here";
-		if (*it == LF)
-			std::cout << "LF here";
+	if (DEBUG) {
+		std::cout << GREY << "Received/remaining data:\n";
+		for(it = _data.begin(); it != end; ++it) {
+			if (*it == CR)
+				std::cout << " CR";
+			else if (*it == LF)
+				std::cout << "LF" << *it;
+			else
+				std::cout << *it;
+		}
+		std::cout << RESET << std::endl;
 	}
-	std::cout << RESET << std::endl;
 }
 
 RequestParser::e_steps RequestParser::step(void) {
@@ -58,6 +61,18 @@ RequestParser::e_parser_error RequestParser::error(void) {
 	return this->_error;
 }
 
+
+
+//Initial lines and headers should end in CRLF, though you should gracefully handle lines ending in just LF.
+// Method names are always uppercase
+// URI enconding (tb serve pra POST) - não entendi se posso receber https
+// URI normalization:
+// normalizing the scheme and host to lowercase
+// normalizing the port to remove any leading zeros.
+// If port is elided from the URI, the default port for that scheme is used
+// o host pode ser um IP, que pode estar em ipv4 ou ipv6...
+// It is RECOMMENDED that all senders and recipients support, at a minimum, URIs with lengths of 8000 octets in protocol elements.
+// The HTTP version always takes the form "HTTP/x.x", uppercase
 void RequestParser::first_line(void) {
 	std::string method;
 	std::string uri;
@@ -65,20 +80,33 @@ void RequestParser::first_line(void) {
 	std::string version;
 	std::vector<char>::iterator it;
 
+	// separar os parsers para evitar receber um monte de dado inválido sem CRLF
+	log.debug("first_line");
 	if (_found_EOL()) {
 		log.debug("Found EOL");
 		for (it = _data.begin(); it != _data_it && *it != SP; ++it)
 			method.push_back(*it);
 		log.debug(method);
-		for (it = _data.begin(); it != _data_it && *it != SP; ++it)
+		while (*it == SP)
+			++it;
+		for (; it != _data_it && *it != SP; ++it)
 			uri.push_back(*it);
 		log.debug(uri);
-		for (it = _data.begin(); it != _data_it && *it != SP; ++it)
+		while (*it == SP)
+			++it;
+		for (; it != _data_it && *it != '/'; ++it)
 			protocol.push_back(*it);
 		log.debug(protocol);
-		for (it = _data.begin(); it != _data_it; ++it)
+		++it;
+		for (; it != _data_it; ++it)
 			version.push_back(*it);
 		log.debug(version);
+		_step = HEADER;
+		_data.erase(_data.begin(), _data_it + 2);
+		_result.insert(t_result_pair("method", method));
+		_result.insert(t_result_pair("uri", uri));
+		_result.insert(t_result_pair("protocol", protocol));
+		_result.insert(t_result_pair("version", version));
 	}
 }
 
@@ -87,11 +115,12 @@ bool RequestParser::_found_EOL(void) {
 
 	for(_data_it = _data.begin(); _data_it != end; ++_data_it) {
 		if (*_data_it == CR) {
-			log.debug("\nCR here");
+			// log.debug("found CR");
 			break;
 		}
 		if (*_data_it == LF) {
-			log.debug("\nLF here");
+			// log.debug("found LF without CR");
+			// talvez o CR não seja necessário; checar na RFC se pode aceitar só o \n
 			_error = LF_WITHOUT_CR;
 			_step = END;
 			return false;
@@ -101,7 +130,8 @@ bool RequestParser::_found_EOL(void) {
 		return false;
 	if (*_data_it == CR) {
 		if (*(_data_it + 1) == LF) {
-			_step = CRLF;
+			// log.debug("found LF");
+			// _step = CRLF;
 			return true;
 		}
 		else if (_data_it + 1 == end)
