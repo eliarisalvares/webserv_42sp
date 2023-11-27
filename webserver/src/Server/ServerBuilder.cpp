b9a08bebfb4eccdef2a3b6829afc7a216f3eaccb
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-11-27 14:26:28 -0300

    feat: checking valid port

--- webserver/src/Server/ServerBuilder.cpp
+++ webserver/src/Server/ServerBuilder.cpp
@@ -1,11 +1,30 @@
 #include "ServerBuilder.hpp"
 
+int	getPort(std::string line) {
+	std::string	info;
+	int			port;
+
+	info = line.substr(7);
+
+	std::istringstream	sPort(info) ;
+	sPort >> port;
+	return port;
+}
+
 void	getServerInfo(std::vector<std::string> input, int index) {
 	for (size_t i = index; i < input.size(); i++) {
 		if (input[i].substr() == "server {")
 			i++ ;
 		if (input[i].substr() == "}")
 			break ;
-		std::cout << "line " << i << " input : " << input[i] << std::endl;
+		if (input[i].substr(0, 7) == "listen ") {
+			int port = getPort(input[i]);
+			if (!port)
+				throw PortNotFoundErrorExeption();
+		}
 	}
+}
+
+const char* PortNotFoundErrorExeption::what() const throw() {
+	return ("Wrong port input");
 }
