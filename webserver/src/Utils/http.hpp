4525dec8fa1c3c8a8023103f56d9d901a0489917
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-11-27 22:18:31 -0300

    http namespace

---
+++ webserver/src/Utils/http.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 21:13:58 by sguilher          #+#    #+#             */
+/*   Updated: 2023/11/27 22:05:16 by sguilher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HTTP_HPP
+# define HTTP_HPP
+
+namespace http {
+	typedef enum e_status {
+		CONTINUE = 100,
+		OK = 200,
+		CREATED = 201,
+		ACCEPTED = 202,
+		NO_CONTENT = 204,
+		MOVED_PERMANENTLY = 301,
+		BAD_REQUEST = 400,
+		UNAUTHORIZED = 401,
+		FORBIDDEN = 403,
+		NOT_FOUND = 404,
+		METHOD_NOT_ALLOWED = 405,
+		NOT_ACCEPTABLE = 406,
+		REQUEST_TIMEOUT = 408,
+		CONFLICT = 409,  // checar se vamos usar
+		LENGTH_REQUIRED = 411,  // checar se vamos usar
+		CONTENT_TOO_LARGE = 413,
+		URI_TOO_LONG = 414,
+		UNSUPPORTED_MEDIA_TYPE = 415,
+		// 422 Unprocessable Content - erro de código em python (cgi gera automático?)
+		INTERNAL_SERVER_ERROR = 500,
+		HTTP_VERSION_NOT_SUPPORTED = 505,
+	}			t_status;
+}
+
+#endif
+
+
