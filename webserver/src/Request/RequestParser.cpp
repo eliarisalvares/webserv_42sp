dcf6ecac413ca27ddbd2c677c8c33936edeaa17f
eliaris.alvares <jenkins@fs-gemini.iam.gserviceaccount.com>
2023-11-28 21:36:17 -0300

    feat: refactoring CGI and resolving memory leaks

--- webserver/src/Request/RequestParser.cpp
+++ webserver/src/Request/RequestParser.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RequestParser.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 21:21:48 by sguilher          #+#    #+#             */
-/*   Updated: 2023/12/06 15:12:12 by sguilher         ###   ########.fr       */
+/*   Updated: 2023/12/06 21:03:04 by feralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
