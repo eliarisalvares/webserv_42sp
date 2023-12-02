61537787b2daf975982401491af3a1f39afc310c
ferfahl <fe.alves.fahl@gmail.com>
2023-12-02 20:33:22 -0300

    chore: removing unnecessary for from obtain's functions

---
+++ webserver/src/Server/server_builder.cpp
@@ -0,0 +1,156 @@
+#include "server_builder.hpp"
+
+int	obtainPort(std::vector<std::string> input, int index) {
+	int		port;
+	Logger	log;
+
+	if (input[index].substr(0, 7) == "listen ") {
+		port = ftstring::strtoi(input[index].substr(7));
+		if (port < 1 || port > 65535)
+			throw PortNotFoundErrorExeption();
+		if (port < 1024)
+			throw PortNeedsSudoExeption();
+		log.debug("Port successfully setted from .conf file.");
+	}
+	return (port);
+}
+
+// Sets the maximum body size for client requests.
+// Megabytes, M, and Kilobytes, K, are the accepted units.
+int	obtainBodySize(std::vector<std::string> input, int index) {
+	int			bodySize;
+	std::string	bodySizeString;
+	Logger		log;
+
+	if (input[index].substr(0, 21) == "client_max_body_size ") {
+		bodySizeString = input[index].substr(21);
+		//read size, obtain M or K -> 1M is the limit
+		//check string for the letter -> give error if letter is different than M or K
+		//if bigger than 1M -> give error
+		// 1K is the minimal size
+		//range from 1K to 1000K -> pass this info ahead
+		bodySize = ftstring::strtoi(bodySizeString);
+		log.debug("client_max_body_size setted from .conf file.");
+	}
+	return (bodySize);
+}
+
+std::string	obtainRoot(std::vector<std::string> input, int index) {
+	std::string	root;
+	Logger		log;
+
+	if (input[index].substr(0, 5) == "root ") {
+		root = input[index].substr(5);
+		//check if root makes sense/exists
+		log.debug("Root setted from .conf file.");
+	}
+	return (root);
+}
+
+std::vector<std::string>	obtainCGI(std::vector<std::string> input, int index) {
+	std::string					name;
+	std::vector<std::string>	serverName;
+	Logger						log;
+
+	if (input[index].substr(0, 4) == "cgi ") {
+		name = input[index].substr(4);
+		serverName = ftstring::split(name, ' ');
+		if (serverName.size() != 2)
+			throw CGIMissconfigurationException();
+		else if (serverName[0] != ".py" || serverName[1] != "python3")
+			throw CGINotSupportedException();
+		log.debug("CGI setted from .conf file.");
+	}
+	return (serverName);
+}
+
+std::vector<std::string>	obtainName(std::vector<std::string> input, int index) {
+	std::string					name;
+	std::vector<std::string>	serverName;
+	Logger						log;
+
+	if (input[index].substr(0, 7) == "server_name ") {
+		name = input[index].substr(7);
+		serverName = ftstring::split(name, ' ');
+		log.debug("Server name setted from .conf file.");
+	}
+	return (serverName);
+}
+
+t_location	obtainLoc(std::vector<std::string> input, int index) {
+	//deal with location :')
+	t_location	location;
+	Logger		log;
+
+	for (size_t i = index; i < input.size(); i++) {
+		if (input[i].substr(0, 7) == "location ") {
+			//do stuff
+		}
+		if (input[index].substr() == "}")
+			break ;
+	}
+	return (location);
+}
+
+std::set<std::string>	obtainMethod(std::vector<std::string> input, int index) {
+	std::set<std::string>		methods;
+	std::vector<std::string>	words;
+	Logger						log;
+
+	if (input[index].substr(0, 16) == "allowed_methods ") {
+		words = ftstring::split(input[index].substr(16), ' ');
+		if (words.size() <= 0 || words.size() >= 4)
+			throw InvalidNbrMethodsException();
+		for (size_t j = 0; j < words.size(); j++) {
+			if (words[j] == "GET" || words[j] == "POST" || words[j] == "DELETE")
+				methods.insert(words[j]);
+			else
+				throw InvalidMethodsException();
+		}
+		log.debug("Allowed Methods setted from .conf file.");
+	}
+	return (methods);
+}
+
+std::pair<int, std::string>	obtainErrorPages(std::vector<std::string> input, int index) {
+	std::pair<int, std::string>	paired;
+	std::vector<std::string>	words;
+	int							nbr;
+	Logger						log;
+
+	if (input[index].substr(0, 11) == "error_page ") {
+		words = ftstring::split(input[index].substr(11), ' ');
+		if (words.size() != 2)
+			throw PortNeedsSudoExeption();
+		nbr = ftstring::strtoi(words[0]); //verificar se o numero de erro é valido
+		//verificar arquivo (words[1]);
+		paired = std::make_pair(nbr, words[1]);
+		log.debug("Error page setted from .conf file.");
+	}
+	return (paired);
+}
+
+//-------------------------------EXCEPTIONS-------------------------//
+const char* PortNotFoundErrorExeption::what() const throw() {
+	return ("Invalid Port found.");
+}
+
+const char* PortNeedsSudoExeption::what() const throw() {
+	return ("Sudo required to run.");
+}
+
+const char* CGIMissconfigurationException::what() const throw() {
+	return ("Wrong number of arguments in CGI.");
+}
+
+const char* CGINotSupportedException::what() const throw() {
+	return ("CGI mode not supported by webserv.");
+}
+
+const char* InvalidNbrMethodsException::what() const throw() {
+	return ("Wrong values for Allowed Methods.");
+}
+
+const char* InvalidMethodsException::what() const throw() {
+	return ("Invalid Allowed Method.");
+}
---
+++ webserver/src/Server/server_getters.cpp
@@ -0,0 +1,70 @@
+#include "Server.hpp"
+
+int	Server::getSocket(void) const {
+	return this->_socket;
+}
+
+int	Server::getBufferSize(void) const {
+	return this->_bufferSize;
+}
+
+int	Server::getPort(void) const {
+	return this->_port;
+}
+
+int	Server::getBodySize(void) const {
+	return this->_client_max_body_size;
+}
+
+std::vector<std::string>	Server::getCGI(void) const {
+	return this->_cgi;
+}
+
+std::string	Server::getRoot(void) const {
+	return this->_root;
+}
+
+std::string	Server::getUpPath(void) const {
+	return this->_uploadPath;
+}
+
+std::set<std::string>	Server::getMethods(void) const {
+	return this->_allowed_methods;
+}
+
+std::set<std::string>	Server::getIndex(void) const {
+	return this->_index;
+}
+
+std::vector<t_location>	Server::getLocations(void) {
+	return this->_locations;
+}
+
+t_location	Server::getLocations(int index) {
+	return this->_locations[index];
+}
+
+std::vector<std::string>	Server::getName(void) {
+	return this->_server_name;
+}
+
+std::string	Server::getName(int index) {
+	return this->_server_name[index];
+}
+
+std::map<int, std::string>	Server::getErrorPages(void) const {
+	return this->_error_pages;
+}
+
+//---------------------------------------------------/
+std::string	Server::getServerName(void) {
+	return (SERVER_NAME);
+}
+
+std::string	Server::getCurrentPort(void) {
+	return (SERVER_PORT);
+}
+
+std::string	Server::getAllowedMethods(void) {
+	return (METHODS);
+}
