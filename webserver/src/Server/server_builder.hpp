4c4afab22ec42e3e808e04c5cd3b6a477ef4efaa
ferfahl <fe.alves.fahl@gmail.com>
2023-12-06 11:36:54 -0300

    feat: buffer size added fix: cgi changed to bool

--- webserver/src/Server/server_builder.hpp
+++ webserver/src/Server/server_builder.hpp
@@ -20,23 +20,23 @@ typedef struct	s_permissions
 
 typedef struct	s_location
 {
+	bool									cgi; // .py python3
 	std::string								location; // "/"
 	std::string								root; // "/content"
 	std::set<std::string>					allowed_methods; //std::set<std::string> _fill_methods(void)
 	std::set<std::string>					index;
-	// std::set<std::string>					cgi; // .py python3
+	t_permissions							permit;
 	// std::vector<std::string>				http_methods;
 	// std::pair<unsigned int, std::string>	http_redirection;
-	// t_permissions							permit;
-	// std::string								response_is_dir;
 }				t_location;
 
+bool						obtainCGI(std::vector<std::string> input, int index);
 int							obtainPort(std::vector<std::string> input, int index);
 int							obtainBodySize(std::vector<std::string> input, int index);
+int							obtainBufferSize(std::vector<std::string> input, int index);
 std::string					obtainRoot(std::vector<std::string> input, int index);
 std::vector<std::string>	obtainName(std::vector<std::string> input, int index);
 t_location					obtainLoc(std::vector<std::string> input, int index);
-std::vector<std::string>	obtainCGI(std::vector<std::string> input, int index);
 std::set<std::string>		obtainMethod(std::vector<std::string> input, int index);
 std::pair<int, std::string>	obtainErrorPages(std::vector<std::string> input, int index);
 std::set<std::string>		obtainIndex(std::vector<std::string> input, int index);
