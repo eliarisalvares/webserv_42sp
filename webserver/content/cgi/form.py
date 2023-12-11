f7371d60e2da05b065bbbd9c6626cc6cc249fb38
eliaris.alvares <jenkins@fs-gemini.iam.gserviceaccount.com>
2023-12-10 23:39:47 -0300

    feat: post with CGI  and improvements in html

--- webserver/content/cgi/form.py
+++ webserver/content/cgi/form.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 import json
 import os
-import uuid
 
 content = os.environ.get("CONTENT_BODY", None)
 if not content:
-    print("Content-Type: application/json")
-    print()
-    print(json.dumps({"success": False, "error": "No content provided."}))
-    exit(0)
+    with open("content/error_pages/400.html", "r") as f:
+        print(f.read())
 
-new_image_url_key = content.split("&")[0]
-new_image_url = new_image_url_key.split("=")[1]
-new_icon_key = content.split("&")[1]
-new_icon = new_icon_key.split("=")[1]
-new_name_key = content.split("&")[2]
-new_name = new_name_key.split("=")[1]
+try:
+    new_image_url_key = content.split("&")[0]
+    new_image_url = new_image_url_key.split("=")[1]
+    new_icon_key = content.split("&")[1]
+    new_icon = new_icon_key.split("=")[1]
+    new_name_key = content.split("&")[2]
+    new_name = new_name_key.split("=")[1]
 
-print("<div class='card'>")
-print(f"<img src='{new_image_url}' alt='Character 4'>")
-print(f"<img src='{new_icon}' class='icon' alt='Icon'>")
-print(f"<div class='card-title'>{new_name}</div>")
-print("</div>")
+    html_content = "<div class='card'>"
+    html_content += f"<img src='{new_image_url}' alt='Character 4'>"
+    html_content += f"<img src='{new_icon}' class='icon' alt='Icon'>"
+    html_content += f"<div class='card-title'>{new_name}</div>"
+    html_content += "</div>"
+    print(html_content)
+
+    file = open("content/database/characters.html", "a")
+    file.write(html_content)
+    file.close()
+
+except:
+    with open("content/error_pages/400.html", "r") as f:
+        print(f.read())
+
 
