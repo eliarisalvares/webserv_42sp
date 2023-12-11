5e9056628345201d3bbe76cd2a8114ce23e4ec46
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-12-11 16:06:51 -0300

    tests: autoindex

--- tests/testes/integration/5_autoindex/on/test.py
+++ tests/testes/integration/5_autoindex/on/test.py
@@ -8,4 +8,8 @@ response = integration.get(URL)
 
 assert response.status_code == 200
 
-assert '<html><head><title>Index of /</title></head><body><h1>Index of /</h1><hr><pre><a href=".">.</a>' in response.text
+assert '<a href="..">..</a>' in response.text
+assert '<a href=".">.</a>' in response.text
+assert '<a href="hello">hello</a>' in response.text
+assert '<a href="hello.html">hello.html</a>' in response.text
+assert '<a href="images">images</a>' in response.text
